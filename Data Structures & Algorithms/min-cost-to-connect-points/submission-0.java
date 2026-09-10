ec8e2880517f2658af6541afccba6ff9eaa8def1
Akash Kushwaha <103249843+akashkuushwahaa@users.noreply.github.com>
2026-09-10 18:55:45 +0530

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+
+        int n = points.length;
+        int[] minDist = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            minDist[i] = Integer.MAX_VALUE;
+        }
+
+        minDist[0] = 0;
+        boolean[] used = new boolean[n];
+        int totalCost = 0;
+
+        for (int count = 0; count < n; count++) {
+            int u = -1;
+            for (int i = 0; i < n; i++) {
+                if (!used[i] && (u == -1 || minDist[i] < minDist[u])) {
+                    u = i;
+                }
+            }
+            used[u] = true;
+            totalCost += minDist[u];
+
+            for (int v = 0; v < n; v++) {
+                if (!used[v]) {
+                    int distance =
+                        Math.abs(points[u][0] - points[v][0])
+                        + Math.abs(points[u][1] - points[v][1]);
+                    minDist[v] = Math.min(minDist[v], distance);
+                }
+            }
+        }
+
+        return totalCost;
+    }
+}
